15ea6821d6929326abbbe0e84dc83ea2ebf840ad
ZenHasan <zenhasan255@gmail.com>
2024-07-03 00:40:25 +0700

    first commit

---
+++ src/main/java/com/juaracoding/utils/ScenarioTests.java
@@ -0,0 +1,20 @@
+package com.juaracoding.utils;
+
+public enum ScenarioTests {
+
+    T1("Successful login with valid credentials"),
+    T2("Failed login with valid credentials"),
+    T3("Add a product to the cart successfully"),
+    T4("Successfully checkout a product");
+    // T1......> T100
+
+    private String scenarioTestName;
+
+    ScenarioTests(String value){
+        scenarioTestName = value;
+    }
+
+    public String getScenarioTestName(){
+        return scenarioTestName;
+    }
+}
